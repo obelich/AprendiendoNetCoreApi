b16d80453dd49fa1dd0879d3d1510ae1d6087456
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2019-05-24 15:56:07 -0600

    WIP JWT

--- Startup.cs
+++ Startup.cs
@@ -70,11 +70,9 @@ namespace DatingApp.API
                         }
                     });
                 });
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                // app.UseHsts();
+
             }
 
-            // app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
